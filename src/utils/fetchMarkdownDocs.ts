4220d6b6ee9a39cb172a3f33db031effc1a553b7
nmstrand <nmstrand@gmail.com>
2022-08-30 12:28:43 -0500

    moved docs to lib and adjusted api to route to /docs/...

--- src/utils/fetchMarkdownDocs.ts
+++ src/utils/fetchMarkdownDocs.ts
@@ -1,15 +1,15 @@
 export const fetchMarkdownDocs = async () => {
-	const allDocFiles = import.meta.glob('/src/routes/docs/*.md');
+	const allDocFiles = import.meta.glob('/src/lib/docs/*.md');
 	const iterableDocFiles = Object.entries(allDocFiles);
 
 	const allDocs = await Promise.all(
 		iterableDocFiles.map(async ([path, resolver]) => {
 			const { metadata } = await resolver();
-			const postPath = path.slice(11, -3);
+			const postPath = path.slice(13, -3);
 
 			return {
 				frontmatter: { ...metadata.frontmatter },
-				path: postPath
+				path: `docs${postPath}`
 			};
 		})
 	);
