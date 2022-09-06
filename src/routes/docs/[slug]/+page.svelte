3be8d4bdb326779a3c3c2b227ee4b02af32ebe94
nmstrand <nmstrand@gmail.com>
2022-09-06 13:21:27 -0500

    updated docPath to retrieve from page params

--- src/routes/docs/[slug]/+page.svelte
+++ src/routes/docs/[slug]/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import type { PageData } from '@sveltejs/kit/types/internal';
+	import { page } from '$app/stores';
 
 	import type { FM } from '$types/types-load-menu-paths';
 
@@ -11,7 +12,7 @@
 
 	export let data: DocData;
 
-	const docPath = data.frontmatter.title.split(' ').join('-')
+	const docPath = $page.params.slug
 	const hackWrapper = '<div class="doc-content s-Ho9MrNAHWzxw"></div>'.length + 1;
 </script>
 
@@ -22,8 +23,9 @@
 {:else}
 	<h2>This page needs some work</h2>
 	<p>
-		Contribute to the documentation <a href={`https://github.com/commonroom/a11y-docsite/blob/master/src/lib/docs/${docPath}.md`} target="_blank"
-			>on Github</a
+		Contribute to the documentation <a
+			href={`https://github.com/commonroom/a11y-docsite/blob/master/src/lib/docs/${docPath}.md`}
+			target="_blank">on Github</a
 		>
 	</p>
 {/if}
